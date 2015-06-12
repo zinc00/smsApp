095b06ba3e5c8bb2833f2d6f8cf8639ab4ac33c1
avifried <avi@innovid.com>
2015-06-12 23:37:48 +0300

    added angularjs

--- js/script.js
+++ js/script.js
@@ -1,8 +1,16 @@
 /**
  * Created by avi on 11/06/2015.
  */
+(function(){
 
+    var smsAppModule = angular.module('smsApp', [])
+        .controller('MainController', ['$scope', function($scope){
+            var fileHandler = function(){
+                $scope.tmp = "tmp";
+                console.log($scope.tmp)
+            };
+            fileHandler();
+        }]);
 
-var fileHandler = function(file){
+})();
 
-};
